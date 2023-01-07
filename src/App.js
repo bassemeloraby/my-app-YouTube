d06949cc6bd77b8fb3574f149cd61d63b6198bfd
bassem eloraby <BASEM12683@GMAIL.COM>
2023-01-07 20:19:17 +0300

    delete posts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret"
+    },
+    { id: 2, name: "Ervin Howell", username: "Antonette" },
+    { id: 3, name: "Clementine Bauch", username: "Samantha" }
+  ]);
+  //delete
+  const deleteHandler = (id) => {
+   
+    setUsers(users.filter((user) => user.id !== id));
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex">
+      {users.map((user) => (
+        <div className="card m-2" style={{ width: "200px" }} key={user.id}>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{user.name}</li>
+          </ul>
+          <button type="button" class="btn btn-dark" onClick={() => deleteHandler(user.id)}>x</button>
+        </div>
+      ))}
     </div>
   );
 }
