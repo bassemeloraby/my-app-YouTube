7934e4d994b63ca98185b019d69786db27e63059
bassem eloraby <BASEM12683@GMAIL.COM>
2023-01-09 01:02:38 +0300

    8-1

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Users = ({ users, deleteHandler }) => {
+  const [updateState, setUpdateState] = useState(-1);
+  const EditHandler = (id) => {
+    console.log(id);
+    setUpdateState(id);
+  };
   return (
-    <div className="container d-flex">
+    <div className="container row justify-content-center">
       {users.map((user) => (
-        <div className="card m-2" style={{ width: '200px' }} key={user.id}>
+        <div
+          className="card m-2 col-3"
+          style={{ width: '200px' }}
+          key={user.id}
+        >
           <ul className="list-group list-group-flush">
             <li className="list-group-item">{user.name}</li>
           </ul>
           <button
             type="button"
-            class="btn btn-dark"
+            className="btn btn-dark"
             onClick={() => deleteHandler(user.id)}
           >
             x
+          </button>
+          <button
+            type="button"
+            class="btn btn-success"
+            onClick={() => EditHandler(user.id)}
+          >
+            Edit
           </button>
         </div>
       ))}
